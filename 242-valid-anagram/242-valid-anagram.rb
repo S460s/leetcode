7a73d1535c93e7493e6b65074a8c30f231f283ef
S460s <66819532+S460s@users.noreply.github.com>
2022-06-11 22:49:10 +0300

    Time: 156 ms (74.62%), Space: 211.6 MB (82.82%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.rb
@@ -0,0 +1,16 @@
+# @param {String} s
+# @param {String} t
+# @return {Boolean}
+def is_anagram(s, t)
+    return false if s.length != t.length
+    
+    s_hash = Hash.new(0)
+    t_hash = Hash.new(0)
+    
+    s.length.times do |i| 
+        s_hash[s[i]] += 1
+        t_hash[t[i]] += 1
+    end
+    
+    s_hash === t_hash
+end
